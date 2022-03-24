340e8ff12d1c500ee84df529f35a345b76eccdb6
XeniaSv <savelicheva123@gmail.com>
2022-03-24 23:37:03 +0300

    [user-service] Add isAdmin field to UserDto

--- dtos/authDtos/UserDto.js
+++ dtos/authDtos/UserDto.js
@@ -2,10 +2,12 @@ module.exports = class UserDto {
     id;
     username;
     email;
+    isAdmin;
 
     constructor(model) {
         this.id = model._id;
         this.username = model.username;
         this.email = model.email;
+        this.isAdmin = model.isAdmin;
     }
 }
