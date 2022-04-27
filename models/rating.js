e9bf958c6e960c2b9bdc2fc004a67b066db17edf
XeniaSv <savelicheva123@gmail.com>
2022-04-27 21:37:39 +0300

    [rating-model] Change ref to refPath

--- models/rating.js
+++ models/rating.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const RatingModel = mongoose.Schema({
     user: {type: String, ref: 'User', require: true},
-    item: {type: String, ref: 'onItem', require: true},
+    item: {type: String, refPath: 'onItem', require: true},
     onItem: {type: String, enum: ['Movie', 'Series', 'Book'], require: true},
     rate: {type: Number, min: 1, max: 5, require: true}
 }, {timestamps: true});
